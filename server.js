59c6d9ac6ed2d393508b201ea0b18268563b3a3b
Stanislav Mekhonoshin <ejabberd@gmail.com>
2014-06-22 01:20:58 +0400

    ConnectionPool для обработки соединений, вынос хелперов в модуль

--- server.js
+++ server.js
@@ -1,43 +1,18 @@
 // http://ejohn.org/blog/ecmascript-5-strict-mode-json-and-more/
 "use strict";
-
-// Optional. You will see this name in eg. 'ps' or 'top' command
 process.title = 'node-chat';
 
-var colors = require('./lib/colors_repository.js')
-var colorsRepository = new colors.repository();
-
-// Port where we'll run the websocket server
+var core = require('./lib/core.js');
 var webSocketsServerPort = 1337;
-
-// websocket and http servers
 var webSocketServer = require('websocket').server;
 var http = require('http');
 
 /**
- * Global variables
- */
-// latest 100 messages
-var history = [ ];
-// list of currently connected clients (users)
-var clients = [ ];
-
-/**
- * Helper function for escaping input strings
- */
-function htmlEntities(str) {
-    return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;')
-                      .replace(/>/g, '&gt;').replace(/"/g, '&quot;');
-}
-
-/**
  * HTTP server
  */
-var server = http.createServer(function(request, response) {
-    // Not important for us. We're writing WebSocket server, not HTTP server
-});
+var server = http.createServer(function(request, response) {});
 server.listen(webSocketsServerPort, function() {
-    console.log((new Date()) + " Server is listening on port " + webSocketsServerPort);
+  console.log((new Date()) + " Server is listening on port " + webSocketsServerPort);
 });
 
 /**
@@ -51,70 +26,7 @@ var wsServer = new webSocketServer({
 
 // This callback function is called every time someone
 // tries to connect to the WebSocket server
+var connectionPool = new core.ConnectionPool();
 wsServer.on('request', function(request) {
-    console.log((new Date()) + ' Connection from origin ' + request.origin + '.');
-
-    // accept connection - you should check 'request.origin' to make sure that
-    // client is connecting from your website
-    // (http://en.wikipedia.org/wiki/Same_origin_policy)
-    var connection = request.accept(null, request.origin); 
-    // we need to know client index to remove them on 'close' event
-    var index = clients.push(connection) - 1;
-    var userName = false;
-    var userColor = false;
-
-    console.log((new Date()) + ' Connection accepted.');
-
-    // send back chat history
-    if (history.length > 0) {
-        connection.sendUTF(JSON.stringify( { type: 'history', data: history} ));
-    }
-
-    // user sent some message
-    connection.on('message', function(message) {
-        if (message.type === 'utf8') { // accept only text
-            if (userName === false) { // first message sent by user is their name
-                // remember user name
-                userName = htmlEntities(message.utf8Data);
-                // get random color and send it back to the user
-                userColor = colorsRepository.getColor();
-                connection.sendUTF(JSON.stringify({ type:'color', data: userColor }));
-                console.log((new Date()) + ' User is known as: ' + userName
-                            + ' with ' + userColor + ' color.');
-
-            } else { // log and broadcast the message
-                console.log((new Date()) + ' Received Message from '
-                            + userName + ': ' + message.utf8Data);
-
-                // we want to keep history of all sent messages
-                var obj = {
-                    time: (new Date()).getTime(),
-                    text: htmlEntities(message.utf8Data),
-                    author: userName,
-                    color: userColor
-                };
-                history.push(obj);
-                history = history.slice(-100);
-
-                // broadcast message to all connected clients
-                var json = JSON.stringify({ type:'message', data: obj });
-                for (var i=0; i < clients.length; i++) {
-                    clients[i].sendUTF(json);
-                }
-            }
-        }
-    });
-
-    // user disconnected
-    connection.on('close', function(connection) {
-        if (userName !== false && userColor !== false) {
-            console.log((new Date()) + " Peer "
-                + connection.remoteAddress + " disconnected.");
-            // remove user from the list of connected clients
-            clients.splice(index, 1);
-            // push back user's color to be reused by another user
-            colorsRepository.returnColor(userColor);
-        }
-    });
-
+  connectionPool.newConnection(request);
 });
