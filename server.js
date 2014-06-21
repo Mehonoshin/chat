80809f637c6832ef16f6c9eb9e834743e6b01e5d
Stanislav Mekhonoshin <ejabberd@gmail.com>
2014-06-22 01:46:26 +0400

    Модель клиента + чистки кода

--- server.js
+++ server.js
@@ -2,31 +2,23 @@
 "use strict";
 process.title = 'node-chat';
 
-var core = require('./lib/core.js');
-var webSocketsServerPort = 1337;
+var core            = require('./lib/core.js');
+var http            = require('http');
 var webSocketServer = require('websocket').server;
-var http = require('http');
 
-/**
- * HTTP server
- */
-var server = http.createServer(function(request, response) {});
+var webSocketsServerPort = 1337;
+
+var connectionPool = new core.ConnectionPool();
+var server         = http.createServer(function(request, response) {});
+
 server.listen(webSocketsServerPort, function() {
   console.log((new Date()) + " Server is listening on port " + webSocketsServerPort);
 });
 
-/**
- * WebSocket server
- */
 var wsServer = new webSocketServer({
-    // WebSocket server is tied to a HTTP server. WebSocket request is just
-    // an enhanced HTTP request. For more info http://tools.ietf.org/html/rfc6455#page-6
-    httpServer: server
+  httpServer: server
 });
 
-// This callback function is called every time someone
-// tries to connect to the WebSocket server
-var connectionPool = new core.ConnectionPool();
 wsServer.on('request', function(request) {
   connectionPool.newConnection(request);
 });
