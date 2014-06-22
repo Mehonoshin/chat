a124f9397b58631459b06466a7215a222c35f21c
Stanislav Mekhonoshin <ejabberd@gmail.com>
2014-06-22 14:22:47 +0400

    Server started logger

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ process.title = 'node-chat';
 var core            = require('./lib/core.js');
 var http            = require('http');
 var webSocketServer = require('websocket').server;
+var logger          = require('./lib/logger.js');
 
 var webSocketsServerPort = 1337;
 
@@ -12,7 +13,7 @@ var connectionPool = new core.ConnectionPool();
 var server         = http.createServer(function(request, response) {});
 
 server.listen(webSocketsServerPort, function() {
-  console.log((new Date()) + " Server is listening on port " + webSocketsServerPort);
+  logger.serverStarted(webSocketsServerPort);
 });
 
 var wsServer = new webSocketServer({
