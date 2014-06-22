a7502bbb7c375297c05f2d12abdec57fa243dc79
Stanislav Mekhonoshin <ejabberd@gmail.com>
2014-06-22 16:51:28 +0400

    Chat class

--- server.js
+++ server.js
@@ -3,13 +3,15 @@
 process.title = 'node-chat';
 
 var core            = require('./lib/core.js');
+var Chat            = require('./lib/chat.js').chat;
 var http            = require('http');
 var webSocketServer = require('websocket').server;
 var logger          = require('./lib/logger.js');
 
 var webSocketsServerPort = 1337;
 
-var connectionPool = new core.ConnectionPool();
+var chat           = new Chat();
+var connectionPool = new core.ConnectionPool(chat);
 var server         = http.createServer(function(request, response) {});
 
 server.listen(webSocketsServerPort, function() {
