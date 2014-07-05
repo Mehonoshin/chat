2aff7ae16a931f45d761faa8421108753c5836bd
Stanislav Mekhonoshin <ejabberd@gmail.com>
2014-07-05 13:23:13 +0400

    #3 Api for loading whole history

--- web.js
+++ web.js
@@ -1,8 +1,17 @@
 var express = require('express');
-var app = express();
+var app     = express();
+var Message = require('./lib/message.js').message;
 
 var server = app.listen(3000, function() {
   console.log('Listening on port %d', server.address().port);
 });
 
 app.use(express.static(__dirname + '/public'));
+
+app.get('/api/history', function(req, res) {
+  var messagesHistory = [];
+  Message.loadAll(function(messages) {
+    messagesHistory = messages.reverse();
+    res.send(messagesHistory);
+  });
+});
