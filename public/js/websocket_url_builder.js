28539634a5127ca68acedb444534ef84a3d6779b
Stanislav Mekhonoshin <ejabberd@gmail.com>
2014-07-09 23:31:07 +0400

    Fix web socket connect from client

--- public/js/websocket_url_builder.js
+++ public/js/websocket_url_builder.js
@@ -1,9 +1,6 @@
 function WebsocketUrlBuilder() {
   this.url = function() {
-    var path = 'ws://' + window.location.hostname;
-    if (window.location.hostname == 'localhost') {
-      path = path + ':1337';
-    }
+    var path = 'ws://' + window.location.hostname + ':1337';
     return path;
   }
 };
